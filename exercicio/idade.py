a431c9186a03ce9219e488ad2d592a6fcaa89cb0
JoaoVitorSantos <joaovitorsantosrhcp@gmail.com>
2024-02-29 20:32:52 -0300

    o codigo da idade precisa ser concertado ps: JONAS MANGOU DE MIM

---
+++ exercicio/idade.py
@@ -0,0 +1,18 @@
+n1 =  int(input("Digite um valor: "))
+n2 = int(input("Digite o segundo valor: "))
+n3 = int(input("Digite o terceiro valor: "))
+
+if n1 > n2:
+    print(n1)
+elif n1 > n3:
+    print(n1)
+elif n2 > n1:
+    print(n2)
+elif n2 > n3:
+    print(n2)
+elif n3 > n1:
+    print(n3)
+elif n3 > n2:
+    print(n3)
+
+
