e96d628318356e5e88edc5930185ba6589c8c0a3
JoaoVitorSantos <joaovitorsantosrhcp@gmail.com>
2024-03-04 19:44:39 -0300

    alteração no código python idade

--- exercicio/idade.py
+++ exercicio/idade.py
@@ -2,17 +2,12 @@ n1 =  int(input("Digite um valor: "))
 n2 = int(input("Digite o segundo valor: "))
 n3 = int(input("Digite o terceiro valor: "))
 
-if n1 > n2:
+if n1 > n2 and n1 > n3:
     print(n1)
-elif n1 > n3:
-    print(n1)
-elif n2 > n1:
+elif n2 > n1 and n2 > n3:
     print(n2)
-elif n2 > n3:
-    print(n2)
-elif n3 > n1:
+elif n3 > n1 and n3 > n2:
     print(n3)
-elif n3 > n2:
-    print(n3)
+
 
 
