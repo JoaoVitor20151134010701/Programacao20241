8e794ce76fb190f00fd25845a91e3f3ab80df944
JoaoVitorSantos <joaovitorsantosrhcp@gmail.com>
2024-03-07 20:24:44 -0300

    Concerto do codigo da atividade

--- exercicio/Atividade/FOR/AtvStringFor.py
+++ exercicio/Atividade/FOR/AtvStringFor.py
@@ -5,7 +5,7 @@ usado no programa.O programa deverá imprimir o padrão:
 Nome<primeiro nome><ultimo nome>
 Data de nascimento<data de nascimento>
 '''
-x =  input("Digite seu Nome e Data de Nascimento: ")
+x =  input("Digite seu Nome e Data de Nascimento ( separando a data por barras): ")
 z = x.split()
 
 print(z[0], z[-2])
