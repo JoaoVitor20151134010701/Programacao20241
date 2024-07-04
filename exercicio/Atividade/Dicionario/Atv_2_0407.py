43b9745423a2e62a914b37f37eb3f1f056803bf3
Joao Vitor Santos <joaovitorsantosrhcp@gmail.com>
2024-07-04 20:33:30 -0300

    AULA 04

---
+++ exercicio/Atividade/Dicionario/Atv_2_0407.py
@@ -0,0 +1,6 @@
+
+'''Faça um script que leia, uma a um, os nomes de pessoa (primeiro nome apenas) e armazeno-os em uma lista. 
+A digitação termina quando você digitar a palavra "FIM". Após os nomes estarem armazenados na lista, faça uma 
+busca e filtragem pra copiar para uma segunda lista apenas os nomes que possuem mais de 6 letras. E, por fim, 
+imprima os nomes filtrados um abaixo do outro.'''
+
