bf6bd7ed64ec9e0c7759c4b3979fef0c61a6abd3
Joao Vitor Santos <joaovitorsantosrhcp@gmail.com>
2024-07-23 20:28:42 -0300

    aula de arquivo

---
+++ exercicio/Arquivo/arquivo_01.py
@@ -0,0 +1,4 @@
+arq = open('meuarquivo.txt', mode='w')
+arq.write('minha primeira linha\n')
+arq.write('Olá Mundo\n')
+arq.close()
---
+++ exercicio/Arquivo/atividade_arq.py
@@ -0,0 +1,36 @@
+'''Faça um script que solicite a entrada do nome e email de pessoas (A
+entrada deve terminar ao ser digitado a string "END"). As informações
+de cada pessoa (Nome e email) devem ser salva em uma lista. E todas
+as listas devem ficar reunidas em uma lista geral. Após isso, o script
+deverá salvar essas informações em um arquivo texto aonde cada
+pessoa deverá ficar armazenada em uma linha do arquivo e o nome e
+email deverão ser separados por vírgula.'''
+
+from asyncore import write
+
+
+def salvarArquivo(x,y):
+    valor = x * y 
+    return valor
+
+
+ListaDados = []
+
+while True:
+    nome = input("Digite seu nome( ao terminar digite 'END'): ")
+    if nome.lower() == 'end':
+        break
+    email = input("Digite Seu email: ")
+    lista = [nome, email]
+    ListaDados.append(lista)
+
+#print(ListaDados)
+
+arq = open('ListadePessoasComEmail.txt','w')
+for p in ListaDados:
+    dado = ",".join(p)
+    arq,write(f"{dado}\n")
+
+arq.close()
+
+print("Arquivo Gerado! ")
