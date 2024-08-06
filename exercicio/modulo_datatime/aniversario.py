1b616b071c74b76bdb6070645d139f64147f963c
Joao Vitor Santos <joaovitorsantosrhcp@gmail.com>
2024-08-06 20:00:57 -0300

    codigo corrigido

--- exercicio/modulo_datatime/aniversario.py
+++ exercicio/modulo_datatime/aniversario.py
@@ -2,8 +2,20 @@
 dd/mm/aaaa) e ele vai lhe informar quantos dias faltam para o seu
 próximo aniversário'''
 
-from datetime import datetime
-import locale
-agora = datetime.now()
-niver = input("Digite a data do seu aniversário utilizando '/' para separá-los: ")
-dataFormatada = datetime.
+from datetime import date
+niver = input("Digite sua data (dia/mes/ano): ")
+
+niver_lista = [int(d) for d in niver.split("/")]
+hoje = date.today()
+niver_esseano = date(hoje.year, niver_lista[1],niver_lista[0])
+
+if hoje > niver_esseano:
+    niver_proxano = date(hoje.year+1,niver_lista[1],niver_lista[0])
+    dif = niver_proxano - hoje
+else:
+    dif = niver_esseano - hoje
+
+if dif.days != 0 :
+    print(f"faltam {dif.days} pro teu aniversario")
+else:
+    print("parabéns para voce! ")    
